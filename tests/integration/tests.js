cfc5bcb8dd70266626fe8c21f7c7dd21a63f843a
Nourhan Elhakim <nourhanmagdy993@gmail.com>
2020-07-21 00:04:38 +0100

    Add test for decimal numbers

--- tests/integration/tests.js
+++ tests/integration/tests.js
@@ -45,4 +45,24 @@ describe('calculator functionality', function() {
     expect(running_total.getAttribute('value')).to.eventually.equal('-3')
   });
 
+  it('should work for decimal numbers', function(){
+    running_total = element(by.css('#running_total'))
+    element(by.css('#number5')).click();
+    element(by.css('#operator_divide')).click();
+    element(by.css('#number2')).click();
+    element(by.css('#operator_equals')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('2.5')
+  });
+
+  // it('should work for long numbers', function(){
+  //   running_total = element(by.css('#running_total'))
+  //   element(by.css('#number2')).click();
+  //   element(by.css('#operator_add')).click();
+  //   element(by.css('#number3')).click();
+  //   element(by.css('#operator_add')).click();
+  //   element(by.css('#number5')).click();
+  //   element(by.css('#operator_equals')).click();
+  //   expect(running_total.getAttribute('value')).to.eventually.equal('10')
+  // });
+
 });
