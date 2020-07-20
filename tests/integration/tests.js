73863718ca978084793fc34e9ab21e100dbff906
Nourhan Elhakim <nourhanmagdy993@gmail.com>
2020-07-21 00:08:57 +0100

    Add test for long numbers

--- tests/integration/tests.js
+++ tests/integration/tests.js
@@ -54,15 +54,23 @@ describe('calculator functionality', function() {
     expect(running_total.getAttribute('value')).to.eventually.equal('2.5')
   });
 
-  // it('should work for long numbers', function(){
-  //   running_total = element(by.css('#running_total'))
-  //   element(by.css('#number2')).click();
-  //   element(by.css('#operator_add')).click();
-  //   element(by.css('#number3')).click();
-  //   element(by.css('#operator_add')).click();
-  //   element(by.css('#number5')).click();
-  //   element(by.css('#operator_equals')).click();
-  //   expect(running_total.getAttribute('value')).to.eventually.equal('10')
-  // });
+  it('should work for long numbers', function(){
+    running_total = element(by.css('#running_total'))
+    element(by.css('#number2')).click();
+    element(by.css('#number3')).click();
+    element(by.css('#number4')).click();
+    element(by.css('#number2')).click();
+    element(by.css('#number8')).click();
+    element(by.css('#number9')).click();
+    element(by.css('#operator_add')).click();
+    element(by.css('#number2')).click();
+    element(by.css('#number8')).click();
+    element(by.css('#number6')).click();
+    element(by.css('#number7')).click();
+    element(by.css('#number1')).click();
+    element(by.css('#number3')).click();
+    element(by.css('#operator_equals')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('521002')
+  });
 
 });
