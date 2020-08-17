f236db0fbc07793b094ffa7de6fae12eb5926eeb
Nourhan Elhakim <nourhanmagdy993@gmail.com>
2020-08-17 21:02:57 +0100

    Refactor the integration test to calculate a numbers higher than a billion

--- tests/integration/tests.js
+++ tests/integration/tests.js
@@ -57,11 +57,15 @@ describe('calculator functionality', function() {
   it('should work for long numbers', function(){
     running_total = element(by.css('#running_total'))
     element(by.css('#number2')).click();
-    element(by.css('#number3')).click();
-    element(by.css('#number4')).click();
-    element(by.css('#number2')).click();
-    element(by.css('#number8')).click();
-    element(by.css('#number9')).click();
+    element(by.css('#number0')).click();
+    element(by.css('#number0')).click();
+    element(by.css('#number0')).click();
+    element(by.css('#number0')).click();
+    element(by.css('#number0')).click();
+    element(by.css('#number0')).click();
+    element(by.css('#number0')).click();
+    element(by.css('#number0')).click();
+    element(by.css('#number0')).click();
     element(by.css('#operator_add')).click();
     element(by.css('#number2')).click();
     element(by.css('#number8')).click();
@@ -70,7 +74,7 @@ describe('calculator functionality', function() {
     element(by.css('#number1')).click();
     element(by.css('#number3')).click();
     element(by.css('#operator_equals')).click();
-    expect(running_total.getAttribute('value')).to.eventually.equal('521002')
+    expect(running_total.getAttribute('value')).to.eventually.equal('2000286713')
   });
 
   it('should return NaN when divide by zero', function(){
